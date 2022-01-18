2709dc233222dc3182e83c07aac44fc72f222ba9
Benjamin Ngo <84323024+benjamin-ngo@users.noreply.github.com>
2022-01-17 23:26:03 -0800

    Provide warnings for Windows devs

---
+++ scripts/preinstall.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+/**********
+* Salesforce Easy Deployments - Templated Script
+* preinstall.js
+* Warns and quits Scripts on non-POSIX systems.
+*  
+* Last Modified: January 2022
+* By: Benjamin Ngo
+**********/
+
+const isWindows = process.platform === "win32";
+if(isWindows){
+    const isWindowsError = "\"Salesforce Easy Deployments\" only supports POSIX compatible systems."
+    const isWindowsSuggestion = "Please try again with alternatives such as \"Windows Subsystem for Linux\".\n"
+    console.error(isWindowsError);
+    console.error(isWindowsSuggestion);
+    process.exitCode = 1;
+}
