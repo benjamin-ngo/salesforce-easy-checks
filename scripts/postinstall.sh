f30d6b408d208fadc1e3ee97b180c22358af7390
Benjamin Ngo <84323024+benjamin-ngo@users.noreply.github.com>
2022-02-17 03:59:12 -0800

    Adjusted postinstall.sh variable and comment headers

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,29 +2,27 @@
 set -euo pipefail
 
 ###########
-# Salesforce Easy Checks - Templated Script
-# postinstall.sh
-# Supports installing non-NPM dependencies (such as PMD) after running "npm install".
-#  
-# Last Modified: January 2022
-# By: Benjamin Ngo
+# @title Salesforce Easy Checks - Templated Script
+# @filename postinstall.sh
+# @description Installs non-NPM dependencies (such as PMD) after running "npm install".
+# @author Benjamin Ngo
 ###########
 
 
-# Sets global variables and working directory. Runs script sanity checks.
+# @description Sets working directory and runs script sanity checks.
 startScript () {
     script_name="$(basename -- "${BASH_SOURCE[0]}")"
-    script_folder_path=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")"; pwd)
-    repo_path="${script_folder_path%*/*}"
-    common_script_name="common.sh"
+    local script_folder_path=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")"; pwd)
+    local repo_path="${script_folder_path%*/*}"
+    local common_script_name="common.sh"
 
-    cd "$repo_path"
+    cd "${repo_path}"
     source "${script_folder_path}/${common_script_name}"
     checkScriptAndSetGlobalVariables
 }
 
-# Quits the script if the correct PMD version is already installed.
-# Some PMD variables are defined in $(checkScriptAndSetGlobalVariables) of common.sh
+
+# @description Quits the script if the correct PMD version is already installed.
 quitIfPmdAlreadyInstalled () {
     # Returns if PMD is not installed.
     if [ ! -f "${pmd_script_path}" ]; then
@@ -32,22 +30,22 @@ quitIfPmdAlreadyInstalled () {
     fi
 
     # Quits if correct PMD version is already installed.
-    pmd_version_output=$( "${pmd_script_path}" pmd --version  )
+    local pmd_version_output
+    pmd_version_output=$( "${pmd_script_path}" pmd --version )
     test "${pmd_version_output#PMD }" = "${pmd_version}"
-    do_pmd_versions_match=$?
-    if [ $do_pmd_versions_match -eq 0 ]; then
-        message_pmd_already_installed="Correct PMD version already exists. Skipping download."
-        echo "${script_name}: ${message_pmd_already_installed}"
+    local do_pmd_versions_match=$?
+    if [ ${do_pmd_versions_match} -eq 0 ]; then
+        local message_pmd_already_installed="Correct PMD version already exists. Skipping download."
+        displayMessage "${message_pmd_already_installed}"
         exit 0
     fi
 }
 
-# Installs PMD. Default behavior does not overwrite files.
-# $pmd_version is defined in $(checkScriptAndSetGlobalVariables) of common.sh
+
+# @description Installs PMD. Default behavior does not overwrite files.
 installPmd () {
-    pmd_download_path="https://github.com/pmd/pmd/releases/download/pmd_releases/${pmd_version}/pmd-bin-${pmd_version}.zip"
-    
-    message_pmd_install_attempt="Attempting to download and install PMD."
+    local pmd_download_path="https://github.com/pmd/pmd/releases/download/pmd_releases/${pmd_version}/pmd-bin-${pmd_version}.zip"
+    local message_pmd_install_attempt="Attempting to download and install PMD."
     echo "${script_name}: ${message_pmd_install_attempt}"
     {
         curl --continue-at - --location --remote-name "${pmd_download_path}" &&
@@ -55,16 +53,17 @@ installPmd () {
         rm "pmd-bin-${pmd_version}.zip"
     } ||
     {
-        error_pmd_install="Error with downloading or unzipping PMD."
-        echo "${script_name}: ${error_pmd_install}"
-        exit 1
+        local error_pmd_install="Error with downloading or unzipping PMD."
+        displayErrorAndQuit "${error_pmd_install}"
     }
 }
 
-# Holds script logic. Can be refactored in future to support more unit testing.
+
+# @description Starts the script.
 main () {
     startScript
     quitIfPmdAlreadyInstalled
     installPmd
 }
 main
+
