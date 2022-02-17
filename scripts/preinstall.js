426d5b41a90713ed56b375fd34eebc8cedb09776
Benjamin Ngo <84323024+benjamin-ngo@users.noreply.github.com>
2022-02-17 03:59:54 -0800

    Adjusted preinstall.js styling

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,19 +1,22 @@
 #!/usr/bin/env node
 
 /**********
-* Salesforce Easy Checks - Templated Script
-* preinstall.js
-* Warns and quits Scripts on non-POSIX systems.
-*  
-* Last Modified: January 2022
-* By: Benjamin Ngo
+* @title Salesforce Easy Checks - Templated Script
+* @filename preinstall.js
+* @description Warns and quits scripts on non-POSIX systems.
+* @author Benjamin Ngo
 **********/
+
+
+const path = require("path");
+const scriptName = path.basename(__filename);
 
 const isWindows = process.platform === "win32";
 if(isWindows){
-    const isWindowsError = "\"Salesforce Easy Deployments\" only supports POSIX compatible systems."
+    const isWindowsError = "\"Salesforce Easy Checks\" only supports POSIX compatible systems."
     const isWindowsSuggestion = "Please try again with alternatives such as \"Windows Subsystem for Linux\".\n"
-    console.error(isWindowsError);
-    console.error(isWindowsSuggestion);
+    console.error(scriptName + ": " + isWindowsError);
+    console.error(scriptName + ": " + isWindowsSuggestion);
     process.exitCode = 1;
 }
+
