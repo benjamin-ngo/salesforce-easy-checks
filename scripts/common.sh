d5436bb0718ec99ec152aec6f1019c0c52f0034f
Benjamin Ngo <84323024+benjamin-ngo@users.noreply.github.com>
2022-02-17 03:57:40 -0800

    Adjusted common.sh variables and comment headers

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,12 +2,10 @@
 set -euo pipefail
 
 ###########
-# Salesforce Easy Checks - Templated Script
-# common.sh
-# Runs script sanity checks and sets common global variables.
-#  
-# Last Modified: January 2022
-# By: Benjamin Ngo
+# @title Salesforce Easy Checks - Templated Script
+# @filename common.sh
+# @description Runs script sanity checks and sets common global variables.
+# @author Benjamin Ngo
 ###########
 
 # In the specific scripts, before sourcing common.sh, please run $(startScript).
@@ -17,35 +15,37 @@ set -euo pipefail
 # Displays an error message and quits the script.
 # All arguments passed to this function will be used as the message text.
 displayErrorAndQuit () {
-    error_message="$*"
+    local error_message="$*"
     echo "${script_name}: ${error_message}"
     exit 1
 }
 
 
-# Sanity check to check if running script is located in the correct repo subfolder.
+# @description Checks if running script is located in the correct repo subfolder.
 isScriptsPathValid () {
     local scripts_folder_expected_name="scripts"
     local running_script_folder_name="${script_folder_path##*/}"
     
-    test "$running_script_folder_name" = "${scripts_folder_expected_name}"
+    test "${running_script_folder_name}" = "${scripts_folder_expected_name}"
     local do_scripts_folders_match=$?
-    if [ $do_scripts_folders_match -eq 1 ]; then
-        error_scripts_folder="Error: Parent directory of Running Script does not appear correct."
+    if [ ${do_scripts_folders_match} -eq 1 ]; then
+        local error_scripts_folder="Error: Parent directory of Running Script does not appear correct."
         displayErrorAndQuit "${error_scripts_folder}"
     fi
 }
 
-# Sanity check to check if Working Directory is the repo directory.
+
+# @description Checks if Working Directory is the repo directory.
 isRepoSetAsWorkingDirectory () (
     local required_repo_file="sfdx-project.json"
-    if [ ! -f "$required_repo_file" ]; then
-        error_repo_folder="Error: Parent directory of directory of Running Script does not appear correct."
+    if [ ! -f "${required_repo_file}" ]; then
+        local error_repo_folder="Error: Parent directory of directory of Running Script does not appear correct."
         displayErrorAndQuit "${error_repo_folder}"
     fi
 )
 
-# If sanity checks pass, sets common global variables.
+
+# @description Runs sanity checks and then sets common global variables.
 checkScriptAndSetGlobalVariables () {
     if isScriptsPathValid && isRepoSetAsWorkingDirectory; then
         pmd_version="6.41.0"
@@ -54,3 +54,4 @@ checkScriptAndSetGlobalVariables () {
         exit 1
     fi
 }
+
